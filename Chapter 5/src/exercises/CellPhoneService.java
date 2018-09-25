96c95606d5c3acb3acb196d020be6bd2b58bfcfa
ftoppert <42647326+ftoppert@users.noreply.github.com>
2018-09-25 13:49:43 -0500

    CellphoneUpdated

--- Chapter 5/src/exercises/CellPhoneService.java
+++ Chapter 5/src/exercises/CellPhoneService.java
@@ -9,34 +9,45 @@ public class CellPhoneService {
 		int minutes;
 		int textMessages ;
 		int dataUsage;
-		String result;
+		String result = null;
 		Scanner input = new Scanner(System.in);
+		System.out.println("How many minutes are you going to use? >> ");
+		minutes = input.nextInt();
+		System.out.println("How much data usage are you planning on using? >>");
+		dataUsage = input.nextInt();
+		System.out.println("How many text messages are hoping to send? >> ");
+		textMessages = input.nextInt();
+		
 
-		if(minutes > 500 && dataUsage == 0 && textMessages == 0);
+		if(minutes > 500 && dataUsage == 0 && textMessages == 0)
 		{
 			result = "You need plan A for $49";
 		}
-		else if(minutes > 500 && dataUsage == 0 && textMessages < 0 );
+		else if(minutes > 500 && dataUsage == 0 && textMessages < 0 )
 		{
 			result = "You need plan B for $55";
 		}
-		else if( minutes < 500 && dataUsage == 0 && textMessages == 100 );
+		else if( minutes < 500 && dataUsage == 0 && textMessages == 100 )
 		{
 			result = "You need plan C for $61";
 		}
-		else if( minutes < 500 && dataUsage == 0 && textMessages < 100 );
+		else if( minutes < 500 && dataUsage == 0 && textMessages < 100 )
 		{
 			result = "You need plan D for $71";
 		}
-		else if( minutes < 500 && dataUsage == 2 && textMessages < 100 );
+		else if( minutes < 500 && dataUsage == 2 && textMessages < 100 )
 		{
 			result = "You need plan E for $79";
 		}
-		else if( minutes < 500 && dataUsage < 2 && textMessages < 100 );
+		else if( minutes < 500 && dataUsage < 2 && textMessages < 100 )
 		{
 			result = "You need plan F for $87";
 		}
 	
+				System.out.print(result);
+
+		}
+	
 	}
 
-}
+
