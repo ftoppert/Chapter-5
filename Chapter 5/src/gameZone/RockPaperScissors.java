5bbc8537a81799e734ad6af5ad813c918c51581b
ftoppert <42647326+ftoppert@users.noreply.github.com>
2018-09-25 13:58:10 -0500

    Rps

--- Chapter 5/src/gameZone/RockPaperScissors.java
+++ Chapter 5/src/gameZone/RockPaperScissors.java
@@ -1,10 +1,22 @@
 package gameZone;
 
+import java.util.Scanner;
+
 public class RockPaperScissors {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int userChoice;
+		int rock = 1;
+		int paper = 2;
+		int scissors = 3;
+		String result;
+		Scanner input = new Scanner(System.in);
+		System.out.println("Rock, Paper,or  Scissors? >> ");
+		userChoice = input.nextInt();
+		
+		if (1 < 3 )
+			result = " Rock wins";
 	}
 
 }
