0fb32b43c4c3b9abc6e6da7e0beef15b893eda64
ftoppert <42647326+ftoppert@users.noreply.github.com>
2018-09-26 09:20:14 -0500

    RockPaperScissors

--- Chapter 5/src/gameZone/RockPaperScissors.java
+++ Chapter 5/src/gameZone/RockPaperScissors.java
@@ -1,22 +1,72 @@
 package gameZone;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class RockPaperScissors {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//1- for Rock
+		//2- for Paper
+		//3- Scissors
+		
 		int userChoice;
-		int rock = 1;
-		int paper = 2;
-		int scissors = 3;
-		String result;
+		String result = null;
 		Scanner input = new Scanner(System.in);
 		System.out.println("Rock, Paper,or  Scissors? >> ");
 		userChoice = input.nextInt();
+
+		Random rand = new Random();
+		int   computerDecision = rand.nextInt(3) + 1;
+
 		
-		if (1 < 3 )
-			result = " Rock wins";
+		if (computerDecision == 3  ) {//scissors
+			if(userChoice == 1) {
+				result = " You win";
+			}
+			else if (userChoice == 2) {//Paper
+				result = "You Lose";
+			}
+			else//Rock
+			{
+				result= "Tie";
+			}	
+		}
+		else if (computerDecision == 1  ) {// rock
+			if(userChoice == 2) {
+				result = " You win";
+			}
+			else if (userChoice == 3) {// scissors
+				result = "You Lose";
+			}
+			else//paper
+			{
+				result= "Tie";
+			}	
+		}
+		else if (computerDecision == 2  ) {// paper
+			if(userChoice == 3) {
+				result = " You win";
+				}
+			else if (userChoice == 1) {// rock
+				result = "You Lose";
+				}
+			else// scissors
+				{
+				result= "Tie";
+				}			
+		}
+		displayMessage(result);
+
 	}
 
+	private static void displayMessage(String result) {
+		// TODO Auto-generated method stub
+		System.out.println(result);
+	}
 }
+
+
+		
+	
