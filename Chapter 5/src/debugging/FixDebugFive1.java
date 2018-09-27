aa69181af418741e152e86e0c02d098ad20ac3c8
ftoppert <42647326+ftoppert@users.noreply.github.com>
2018-09-27 09:45:03 -0500

    debugging 1

--- Chapter 5/src/debugging/FixDebugFive1.java
+++ Chapter 5/src/debugging/FixDebugFive1.java
@@ -1,36 +1,45 @@
 package debugging;
-import javax.swing.JOptionPane;
-public class Debugging1
-{
-
-
-public static void main(String args[]) throws Exception
 // FixDebugFive1
 // Adds your lunch bill
 // Burger and hot dog are $2.59
 // Grilled cheese and fish are $1.99
 // Fries are 89 cents
+
+import javax.swing.JOptionPane;
+public class FixDebugFive1
+{
+   public static void main(String args[]) throws Exception
    {
       final double HIGH_PRICE = 2.59;
       final double MED_PRICE = 1.99;
       final double LOW_PRICE = 0.89;
       String usersChoiceString;
-   double usersChoice;
+      int usersChoice;
       double bill = 0.0;
       usersChoiceString = JOptionPane.showInputDialog(null,
          "Order please/n1 - Burger\2 - Hotdog" +
          "\n3 - Grilled cheese\n4 - Fish sandwich");
-      usersChoice= Integer.parseDouble(usersChoiceString);
-      if (usersChoice = (1 | usersChoice) == 2)
+      usersChoice= Integer(usersChoiceString);
+      if(usersChoice = (1 | usersChoiceString)())== 2)
          bill = bill + HIGH_PRICE;
       else
          bill = bill + MED_PRICE;
       usersChoiceString = JOptionPane.showInputDialog(null,
           "Fries with that?\n1 - Yes\n2 - No");
       usersChoice = Integer.parseInt(usersChoiceString);
- 
-	if (usersChoice =  bill = bill + LOW_PRICE);
+      if (usersChoiceString = 1 != null)
+          bill = bill + LOW_PRICE;
       JOptionPane.showMessageDialog(null,"Bill is " + bill);
 
+}
+
+private static int usersChoiceString() {
+	// TODO Auto-generated method stub
+	return 0;
+}
+
+private static int Integer(String usersChoiceString) {
+	// TODO Auto-generated method stub
+	return 0;
 }
 }
