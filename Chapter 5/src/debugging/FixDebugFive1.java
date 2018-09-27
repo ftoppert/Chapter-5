1bd5b9b6e748f7751acd7909c50c79823b9cc7e4
ftoppert <42647326+ftoppert@users.noreply.github.com>
2018-09-27 09:48:43 -0500

    Debugging 1

--- Chapter 5/src/debugging/FixDebugFive1.java
+++ Chapter 5/src/debugging/FixDebugFive1.java
@@ -20,14 +20,14 @@ public class FixDebugFive1
          "Order please/n1 - Burger\2 - Hotdog" +
          "\n3 - Grilled cheese\n4 - Fish sandwich");
       usersChoice= Integer(usersChoiceString);
-      if(usersChoice = (1 | usersChoiceString)())== 2)
+      if(usersChoice == 2)
          bill = bill + HIGH_PRICE;
       else
          bill = bill + MED_PRICE;
       usersChoiceString = JOptionPane.showInputDialog(null,
           "Fries with that?\n1 - Yes\n2 - No");
       usersChoice = Integer.parseInt(usersChoiceString);
-      if (usersChoiceString = 1 != null)
+      if (usersChoice == 1)
           bill = bill + LOW_PRICE;
       JOptionPane.showMessageDialog(null,"Bill is " + bill);
 
